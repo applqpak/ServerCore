4ef239fdd8d7b2ea099aab90589546f0373e206c
applqpak <applqpak@gmail.com>
2017-01-14 17:51:14 +1100

    Create onPlayerLeaveEvent.php

---
+++ src/ServerCore/events/onPlayerLeaveEvent.php
@@ -0,0 +1,28 @@
+<?php
+
+  namespace ServerCore\events;
+  
+  use pocketmine\event\Listener;
+  use pocketmine\utils\TextFormat as TF;
+  use pockemine\event\player\PlayerQuitEvent;
+  
+  use ServerCore\Main;
+  
+  class onPlayerLeaveEvent implements Listener
+  {
+      private $cfg;
+      private $plugin;
+      
+      public function __construct(Main $plugin)
+      {
+          $this->cfg    = $plugin->join_leave;
+          $this->plugin = $plugin;
+      }
+      
+      public function onPlayerQuit(PlayerQuitEvent $event)
+      {
+          $name = $event->getPlayer()->getName();
+          $quit = str_ireplace(['%player%', '#player#', '{player}'], [$name, $name, $name], $this->cfg->get('leave'));
+          $event->setQuitMessage($quit);
+      }
+  }
